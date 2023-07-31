7524e4005819e34a12e698e096299ef03db75705
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-07-31 12:44:25 -0300

    ajt

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AgendaContext>(options => 
-                    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+builder
+    .Services
+    .AddDbContext<AgendaContext>(options => 
+                    options.UseSqlServer(builder
+                           .Configuration
+                           .GetConnectionString("ConexaoPadrao")));
 
 
 builder.Services.AddControllers();
